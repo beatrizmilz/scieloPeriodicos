ecc66ad492d63ae923773a786fc1cd9b55ca8520
Beatriz Milz <milz.bea@gmail.com>
2024-04-07 19:41:03 -0300

    Explorando os dados - finalmente algo que será útil

--- data-raw/criterios.R
+++ data-raw/criterios.R
@@ -1,16 +1,24 @@
+# O vetor abaixo foi criado manualmente,
+# a partir da observação dos valores.
+# Isso deve ser incrementado com o tempo
+
 criterios_scielo <- tibble::tibble(
   criterio = "sex_gender_issues",
-  nome_criterio = "Sex and Gender Issues",
+  nome_criterio_en = "Sex and Gender Issues",
+  nome_criterio = "Questões de Sexo e Gênero",
   palavras_chave = list(c("Sex and Gender Issues",
                 "Gender and Sex Issues",
-                "Sex and genre issues"))
+                "Sex and genre issues",
+                "Questões de Sexo e Gênero"))
 ) |>
   tibble::add_row(
   criterio = "preprints",
   nome_criterio = "Preprints",
+  nome_criterio_en = "Preprints",
   palavras_chave = list(c("Preprints", "Preprint",
                 "Preprint policy",
-                "Acceptance of Preprint"))
+                "Acceptance of Preprint",
+                "Aceitação de Preprint"))
   )
 
 
