dc99a7d4874d648493a8e1ab1435df071330fc65
Beatriz Milz <milz.bea@gmail.com>
2024-04-07 19:59:34 -0300

    mais critérios

--- data-raw/criterios.R
+++ data-raw/criterios.R
@@ -6,19 +6,73 @@ criterios_scielo <- tibble::tibble(
   criterio = "sex_gender_issues",
   nome_criterio_en = "Sex and Gender Issues",
   nome_criterio = "Questões de Sexo e Gênero",
-  palavras_chave = list(c("Sex and Gender Issues",
-                "Gender and Sex Issues",
-                "Sex and genre issues",
-                "Questões de Sexo e Gênero"))
+  palavras_chave = list(
+    c(
+      "Sex and Gender Issues",
+      "Gender and Sex Issues",
+      "Sex and genre issues",
+      "Questões de Sexo e Gênero"
+    )
+  )
 ) |>
   tibble::add_row(
-  criterio = "preprints",
-  nome_criterio = "Preprints",
-  nome_criterio_en = "Preprints",
-  palavras_chave = list(c("Preprints", "Preprint",
-                "Preprint policy",
-                "Acceptance of Preprint",
-                "Aceitação de Preprint"))
+    criterio = "preprints",
+    nome_criterio = "Preprints",
+    nome_criterio_en = "Preprints",
+    palavras_chave = list(
+      c(
+        "Preprints",
+        "Preprint",
+        "Preprint policy",
+        "Acceptance of Preprint",
+        "Aceitação de Preprint"
+      )
+    )
+  ) |>
+  tibble::add_row(
+    criterio = "open_science",
+    nome_criterio = "Conformidade com a Ciência Aberta",
+    palavras_chave = list(
+      c(
+        "Conformidade com a Ciência Aberta",
+        "Ciência aberta",
+        "Open Science",
+        "Formulário de Conformidade com a Ciência Aberta",
+        "Boas Práticas de Ciência Aberta",
+        "Incentivo à Ciência Aberta"
+      )
+    )
+  ) |>
+  tibble::add_row(
+    criterio = "ethics",
+    nome_criterio = "Ética",
+    palavras_chave = list(
+      c(
+        "Ética e integridade",
+        "Ética",
+        "Ético",
+        "Recomendações Éticas",
+        "Considerações Éticas e Integridade em Pesquisa",
+        "Ética na pesquisa com seres humanos e animais",
+        "Código de Ética",
+        "COMISSÃO DE ÉTICA",
+        "Comitê de ética",
+        "Conduta Ética",
+        "Considerações éticas",
+        "Considerações Éticas e Integridade em Pesquisa",
+        "Considerações Éticas e Legais",
+        "Consultoria de ética em pesquisa",
+        "Declaração de ética",
+        "Declaração de ética e boas práticas",
+        "Declaração de ética e de boas práticas de publicação",
+        "Declaração de publicação ética e prática indevidas",
+        "Declaração sobre ética e práticas inadequadas",
+        "Diretrizes éticas",
+        "Diretrizes sobre boas práticas éticas",
+        "Recomendações Éticas",
+        "Pesquisa em Seres Humanos e Experimentais"
+      )
+    )
   )
 
 
--- inst/relatorios/gerar_relatorios.R
+++ inst/relatorios/gerar_relatorios.R
@@ -1,20 +1,24 @@
+devtools::load_all()
+
 criterios <- criterios_scielo$criterio
 
-arquivos <- criterios |>
+arquivos <- criterios[2] |>
   purrr::walk(
     ~ quarto::quarto_render(
     input = here::here("inst", "relatorios", "template_aderencia_criterios.qmd"),
     execute_params = list(criterio = .x),
     output_file = paste0(.x, ".html")
-  ))
+  ), .progress = TRUE)
 
 paste0(arquivos, ".html") |>
-fs::file_move("docs/", overwrite = TRUE)
+fs::file_move("docs/")
 
 
 
 paste0("inst/relatorios/template_aderencia_criterios_files") |>
 fs::dir_copy("docs/", overwrite = TRUE)
+
+# Index ----------
 
 paste0("inst/relatorios/index.html") |>
 fs::file_move("docs/")
