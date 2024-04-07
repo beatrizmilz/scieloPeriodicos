c31025471af1c624b405f7a8d1a50117b503fca0
Beatriz Milz <milz.bea@gmail.com>
2024-04-07 13:21:38 -0300

    exportando um csv dos periódicos ativos na SciELO

---
+++ data-raw/periodicos_ativos.R
@@ -0,0 +1,10 @@
+# Carregar o pacote
+devtools::load_all()
+
+# Listar periódicos ativos
+periodicos_ativos <- listar_periodicos_ativos()
+
+# Salvando no repositório em um arquivo CSV
+readr::write_csv(periodicos_ativos,
+                 file = "inst/periodicos_ativos_SciELO.csv",
+                 quote = "needed")
